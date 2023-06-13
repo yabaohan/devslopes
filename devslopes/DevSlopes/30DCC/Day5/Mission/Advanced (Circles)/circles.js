12573ae5263f475da057ace2d3905f8bb5d05220
Hans Joshua D. Yabao <hyabao0518@yahoo.com>
2023-06-13 19:52:12 -0400

    Delete devslopes/DevSlopes directory

--- devslopes/DevSlopes/30DCC/Day5/Mission/Advanced (Circles)/circles.js
+++
@@ -1,2 +0,0 @@
-//Given r for radius, write an algorithm to determine the area of a circle.
-//Solve for the following values: r15, r128, r7, r6
--- devslopes/DevSlopes/30DCC/Day6/Mission/Advanced (Triangle)/triangle.js
+++
@@ -1,4 +0,0 @@
-//Given the inputs below, write an algorithm to find the area of a triangle
-// length : 125
-// width : 81
-// height: 74
