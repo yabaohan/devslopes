62e20b01effd9682aaaa398850609bfa42eb3fdb
Hans Yabao <hyabao0518@yahoo.com>
2023-06-19 04:15:36 -0400

    added

---
+++ DevSlopes/30DCC/Day12/Missions/Advanced(SumRange)/sumRange.js
@@ -0,0 +1,7 @@
+//Create a function that takes in an array of 2 numbers as a parameter
+//Return the sum of those two numbers, plus all the numbers in between
+
+function sumRange (arr){
+    
+};
+
