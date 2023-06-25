ee78a690466c706b0281532a60560d42fd92a64b
Hans Yabao <hyabao0518@yahoo.com>
2023-06-24 23:08:11 -0400

    added day 15

---
+++ DevSlopes/30DCC/Day13/Missions/Advanced (Decimals)/decimals.js
@@ -0,0 +1,7 @@
+// You are going to create a terminal-based calculator program using Node.
+// Handle remainders properly (modulo).
+//  Example : 9 divided by 4 would  print 2.25 
+ 
+//  Make the program more elegant by allowing the user to enter an entire  operation on one line such as: ‘’6 / 6’’ or ‘’5 * 3’’ 
+//  (Hint use arguments to  solve this)1. 2.Resources for help: https://www.w3schools.com/nodejs/ref_readline.asp 
+// https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input/ https://www.codecademy.com/articles/getting-user-input-in-node-js 
--- my-hobbies/index.js
+++ my-hobbies/index.js
@@ -8,30 +8,53 @@ var rs = require('readline-sync');
 //Store list of user hobbies
 //Print uuuuser hobby list at the end
 var hobbies = [];
-
+var areYouSure = false;
+var moreHobbies = 0;
 var userName = rs.question('What is your name? ');
-console.log('Hi ' + userName + '!');
-var favHobby = rs.question('What\'s youre favorite hobby? ')
-hobbies.push(favHobby);
 
-var moreHobbies = rs.questionInt('How many other hobbies do you have? ')
-if(moreHobbies > 0){
-    //add hobbies
+function getHobbyCount(){
+    moreHobbies =rs.questionInt('How many other hobbies do you have? ');
+}
+
+function addHobbies(numHobbies){
     var counter = 0;
-    while(counter < moreHobbies){
+    while(counter < numHobbies){
         var input = rs.prompt();
         hobbies.push(input);
         counter++;
 
-        if (counter < moreHobbies) {
-            console.log('-- Awesome! "' + input + '"is cool! What\'s the other ' + (moreHobbies - counter) + '? ');
+        if (counter < numHobbies) {
+            console.log('-- Awesome! "' + input + '"is cool! What\'s the other ' + (numHobbies - counter) + '? ');
         } else {
             console.log('I wish I could do ' + hobbies + '...but I\'m not even real.');
         }
     } 
+}
+
+
+console.log('Hi ' + userName + '!');
+
+var favHobby = rs.question('What\'s youre favorite hobby? ')
+hobbies.push(favHobby);
+getHobbyCount();
+
+
+if(moreHobbies > 0){
+    console.log('Cool, what are they?')
+    //add hobbies
+  addHobbies(moreHobbies);
 
 } else {
-    var areYouSure = rs.keyInYN('Are you sure?' + hobbies[0] + ' is all you do? ')
+    var areYouSure = rs.keyInYN('Are you sure? ' + hobbies[0] + ' is all you do? ')
+}
+
+if(!areYouSure && hobbies.length <= 1){
+    //add hobbies
+    getHobbyCount();
+    console.log('Cool, what are they?')
+    addHobbies(moreHobbies);
+} else if (areYouSure) {
+    console.log(hobbies + ' is cool, good for you. ');
 }
 
 
