8c8828231a6fd7069236895af018ec16c495948c
Hans Yabao <hyabao0518@yahoo.com>
2023-06-26 19:06:36 -0400

    added day 16

--- DevSlopes/30DCC/Day15/Missions/roots.js
+++ DevSlopes/30DCC/Day15/Missions/roots.js
@@ -1,6 +1,17 @@
-//Make a function from scratch that finds the sqrt of a number num.
-function Roots (num) {
-    while ((num % 10) != 0 ) {
-        
+// //Make a function from scratch that finds the sqrt of a number num.
+function Roots(x) {
+    if (x<=1) {
+        return x;
+    }
+    for ( let i = 2; i <= x; i++){
+        if (i * i === x){
+            return i;
+        }
+        if (i * i >= x) {
+            return i - 1;
+        }
     }
 }
+
+
+console.log(squareWithoutHelp(100));
